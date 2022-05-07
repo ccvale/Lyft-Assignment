60b12bfb80fd169dabd84fdf549b50937586a029
Chris Valente <63901165+ccvale@users.noreply.github.com>
2022-05-07 12:57:10 -0400

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lyft Interview
+
+## Response to the Technical Sample
+
+- Using Python and FastAPI, we accept the `/test` route that accepts a `string_to_cut` argument
+
+- The response object is a `return_string` that contains every third letter of the passed `string_to_cut` argument
+
+- I have also provided a simple HTML page that mimics this functionality
---
+++ lyft.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+class LyftRequest(BaseModel):
+    string_to_cut: str
+
+'''
+The application only needs to do the following:
+Accept a POST request to the route “/test”, which accepts one argument “string_to_cut”
+Return a JSON object with the key “return_string” and a string containing every third
+letter from the original string
+(e.g.) If you POST {"string_to_cut": "iamyourlyftdriver"}, it will return: 
+{"return_string": "muydv"}.
+'''
+
+'''
+Deadline: May 13 8PM
+https://boards.greenhouse.io/lyft/jobs/6063505002?gh_jid=6063505002#app
+'''
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory='templates')
+
+# s = 'iamyourlyftdriver'
+
+def everyThird(s: str):
+    new = ''
+    for i in range(2, len(s), 3):
+        new += s[i]
+    return new
+
+@app.get('/')
+def home(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+@app.post('/test')
+def test(req: LyftRequest):
+    if len(req.string_to_cut) < 3:
+        return {'return_string': 'Error - string not long enough!'}
+    return {'return_string': everyThird(req.string_to_cut)}
